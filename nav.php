75339ef608129a97ee823d46e4264ab2314f75f8
Willielee888 <93363085+Willielee888@users.noreply.github.com>
2021-10-29 11:44:52 +0800

    updated file
    updated file

---
+++ nav.php
@@ -0,0 +1,10 @@
+<h1>Police Emergency Service System</h1>
+<link href="nav.css" rel="stylesheet" type="text/css">
+<nav>
+<ul>
+  <li><a class="active" href="logcall.php">Logcall</a></li>
+  <li><a href="update.php">Update</a></li>
+  <li><a href="">Report</a></li>
+  <li><a href="">History</a></li>
+</ul>
+ </nav>
